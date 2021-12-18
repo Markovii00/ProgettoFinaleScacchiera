c27778ea0dfa8971b55f9dddba65e1c8072d2783
Markovii00 <46714531+Markovii00@users.noreply.github.com>
2021-12-18 13:57:42 +0100

    Test(a di cazzo)

---
+++ test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tomma on 12/18/2021.
+//
+
+#include "test.h"
+
+int main {
+
+};
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by Tomma on 12/18/2021.
+//
+
+#ifndef SCACCHIERA2_TEST_H
+#define SCACCHIERA2_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //SCACCHIERA2_TEST_H
