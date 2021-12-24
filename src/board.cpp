ae3c6ffae8e687bbb21532f5201e88f3ba8bfbb9
zincalex <92792074+zincalex@users.noreply.github.com>
2021-12-24 12:07:34 +0100

    chessman.h new methods scacchiera new ideas *.h improvements and corrections

--- src/board.cpp
+++ src/board.cpp
@@ -13,7 +13,7 @@
 
 board::board()
 {
-        for (unsigned short cRow = 0; cRow < 8; ++cRow)
+    for (unsigned short cRow = 0; cRow < 8; ++cRow)
     {
         for (unsigned short cCol = 0; cCol < 8; ++cCol)
         {
@@ -21,35 +21,34 @@ board::board()
         }
     }
      
-     //allocating all black pawns and pieces
+    //allocating all black pawns and pieces
+    for (unsigned short cCol = 0; cCol < 8; ++cCol)
+    {
+        chessboard[cCol][6] = new pawn('P', cCol, 6);
+    }
+    chessboard[0][7] = new rook('T', 0, 7);
+    chessboard[1][7] = new knight('C', 1, 7);
+    chessboard[2][7] = new bishop('A', 2, 7);
+    chessboard[3][7] = new queen('D');
+    chessboard[4][7] = new king('R');
+    chessboard[5][7] = new bishop('A', 5, 7);
+    chessboard[6][7] = new knight('C', 6, 7);
+    chessboard[7][7] = new rook('T', 7, 7);
 
-     for (unsigned short cCol = 0; cCol < 8; ++cCol)
-     {
-         chessboard[cCol][6] = new pawn('P', cCol, 6);
-     }
-     chessboard[0][7] = new rook('T', 0, 7);
-     chessboard[1][7] = new knight('C', 1, 7);
-     chessboard[2][7] = new bishop('A', 2, 7);
-     chessboard[3][7] = new queen('D');
-     chessboard[4][7] = new king('R');
-     chessboard[5][7] = new bishop('A', 5, 7);
-     chessboard[6][7] = new knight('C', 6, 7);
-     chessboard[7][7] = new rook('T', 7, 7);
+    //allocating all white pawns and pieces
 
-     //allocating all white pawns and pieces
-
-     for (unsigned short cCol = 0; cCol < 8; cCol++)
-     {
-         chessboard[cCol][1] = new pawn('p', cCol, 1);
-     }
-     chessboard[0][1] = new rook('t', 0, 1);
-     chessboard[1][1] = new knight('c', 1, 1);
-     chessboard[2][1] = new bishop('a', 2, 1);
-     chessboard[3][1] = new queen('d');
-     chessboard[4][1] = new king('r');
-     chessboard[5][1] = new bishop('a', 5, 1);
-     chessboard[6][1] = new knight('c', 6, 1);
-     chessboard[7][1] = new rook('t', 7, 1);
+    for (unsigned short cCol = 0; cCol < 8; cCol++)
+    {
+        chessboard[cCol][1] = new pawn('p', cCol, 1);
+    }
+    chessboard[0][1] = new rook('t', 0, 1);
+    chessboard[1][1] = new knight('c', 1, 1);
+    chessboard[2][1] = new bishop('a', 2, 1);
+    chessboard[3][1] = new queen('d');
+    chessboard[4][1] = new king('r');
+    chessboard[5][1] = new bishop('a', 5, 1);
+    chessboard[6][1] = new knight('c', 6, 1);
+    chessboard[7][1] = new rook('t', 7, 1);
 }
 
 board::~board(){
